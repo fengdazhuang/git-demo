d336c74a7a8e00141d735601f54fe4826d2f56bd
fengdazhuang <3172029811@qq.com>
2022-08-09 15:11:12 +0800

    push commit

--- src/com/fzz/git/HelloGit.java
+++ src/com/fzz/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("hello,git!");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push-test");
     }
 }
